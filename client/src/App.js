b4afb2f55f2a670f6478916862835498b2e7a706
Jasyn Marais <jasynmarais@gmail.com>
2019-12-06 10:33:17 +0200

    Add class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchPlayers();
+  }
+
+  fetchPlayers() {
+    axios
+    .get('http://localhost:5000/api/players')
+    .then(res => {
+      this.setState({ players: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Wassup!</h1>
     </div>
   );
+}
 }
 
 export default App;
